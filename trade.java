f648b81ab2d5d4ac509cfef63d3031d98384bee5
rachelteoh1 <22004780@siswa.um.edu.my>
2023-05-11 11:53:44 +0800

    test1

--- trade.java
+++ trade.java
@@ -2,6 +2,6 @@ public class trade {
     public static void main(String[] args) {
         System.out.println("trade");
         System.out.println("123");
-        System.out.println("hi");
+        System.out.println("hello");
     }
 }
