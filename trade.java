d90e4df13961e84bed94aa695f58ab7bc8602051
rachelteoh1 <133184164+rachelteoh1@users.noreply.github.com>
2023-05-24 11:19:50 +0800

    Delete trade.java

--- trade.java
+++
@@ -1,7 +0,0 @@
-public class trade {
-    public static void main(String[] args) {
-        System.out.println("trade");
-        System.out.println("12345");
-        System.out.println("hello world");
-    }
-}
